01fcb496f23325dc298feb2896fa0a477ba98936
PedroMaroal <pedro.maroal@gmail.com>
2024-12-10 18:48:06 +0000

    Add mock test to app.controller.test.ts

--- src/app.controller.test.ts
+++ src/app.controller.test.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { TwoSumLogService } from './twoSumLog.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  //create mock of service for testing
+  const mockTwoSumLogService = {
+    logRequest: jest.fn(),
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
+
+      //Inject the mock service into the controller
+      providers: [
+        {
+          provide: TwoSumLogService,
+          useValue: mockTwoSumLogService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
--- src/twoSum.test.ts
+++ src/twoSum.test.ts
@@ -33,4 +33,22 @@ describe('twoSum function', () => {
     const result = twoSum(array, target);
     expect(result).toEqual({ numberOne: -3, numberTwo: 3 });
   });
+  it('should handle large numbers correctly', () => {
+    const array = [1000000000, 1000000001, 1000000002, 1000000003];
+    const target = 2000000005;
+    const result = twoSum(array, target);
+    expect(result).toEqual({ numberOne: 1000000002, numberTwo: 1000000003 });
+  });
+  it('should handle arrays with duplicate numbers correctly', () => {
+    const array = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const target = 9;
+    const result = twoSum(array, target);
+    expect(result).toEqual({ numberOne: 1, numberTwo: 8 });
+  });
+  it('should handle chosen any number if are repeated', () => {
+    const array = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+    const target = 4;
+    const result = twoSum(array, target);
+    expect(result).toEqual({ numberOne: 2, numberTwo: 2 });
+  });
 });
